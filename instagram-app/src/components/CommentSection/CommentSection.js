959dc42f2716fbdf9821a242c27bcbc6af39f608
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-05-02 08:54:16 -0400

    CommentSection class
    -constructor
    - set state

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -2,3 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Comment from './Comment';
 import CommentInput from './CommentInput';
+
+class CommentSection extends React.Component {
+    constructor(props) {
+        this.state = {
+            comments: props.comments,
+            comment: ''
+        };
+    }
+    
+}
