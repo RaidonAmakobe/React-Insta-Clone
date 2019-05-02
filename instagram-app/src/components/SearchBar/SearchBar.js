dd70e78066874420f2cf36572803fac2e8c481df
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-05-01 23:47:40 -0400

    positioned searchbar

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -34,11 +34,14 @@ class SearchBar extends React.Component {
         <Navbar color="light" light expand="md">
           <NavbarBrand href="/"><img src = "./images/instagram2.png" style = {{width: 30}}/></NavbarBrand>
           <NavbarBrand href="/"><img src = "./images/instagram.png" style = {{width: 130, paddingTop: 5, paddingLeft: 30}}/></NavbarBrand>
+          <Col xs="9">
           <NavbarBrand>
             <InputGroup>
                 <Input placeholder = "Search" />
             </InputGroup>
           </NavbarBrand>
+          </Col>
+          
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
