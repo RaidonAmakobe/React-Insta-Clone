6e63b1ae2b2400f840f702841b62b5f35c109f16
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-05-02 02:12:42 -0400

    Added media queries

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -18,50 +18,71 @@ class PostContainer extends React.Component {
                 <CardHeader><img src = "./images/circleimg.png" style = {{width: 30, paddingRight: 10}}/>mrtestperson</CardHeader>
                 <CardBody>
                   <CardTitle><Image src = "./images/audric.jpg" fluid style = {{width: 450}}/></CardTitle>
-                  <Media query = "(Max width: 400px)">
+                  <Media query = "(max-width: 400px)">
                   <CardText>
                     <small className="text-muted">
-                        <Row style = {{width: 450, marginLeft: 200, marginTop: 10}}>
+                        <Row style = {{width: 450, marginLeft: 10, marginTop: 10}}>
                             <img src = "./images/like.svg" style = {{width: 19}}/>
                             <img src = "./images/chat.svg" style = {{width: 19, marginLeft: 20}}/>
                         </Row>
                     </small>
                     <small className = "text-muted">
-                            <Row style = {{width: 450, marginLeft: 200, marginTop: 10}}>
+                            <Row style = {{width: 450, marginLeft: 10, marginTop: 10}}>
                                 300 Likes
                             </Row>
                            
-                                <InputGroup style = {{width: 450, marginLeft: 200, marginTop: 10}}>
+                                <InputGroup style = {{width: 320, marginLeft: 10, marginTop: 10}}>
                                     <Input placeholder = "Add a comment..." />
-                                </InputGroup>
-                        
-                            
+                                </InputGroup>  
                     </small>
         
             
                   </CardText>
                   </Media>
+
+                  <Media query = "(max-width: 400px)">
                   <CardText>
                     <small className="text-muted">
-                        <Row style = {{width: 450, marginLeft: 520, marginTop: 10}}>
+                        <Row style = {{width: 450, marginLeft: 10, marginTop: 10}}>
                             <img src = "./images/like.svg" style = {{width: 19}}/>
                             <img src = "./images/chat.svg" style = {{width: 19, marginLeft: 20}}/>
                         </Row>
                     </small>
                     <small className = "text-muted">
-                            <Row style = {{width: 450, marginLeft: 520, marginTop: 10}}>
+                            <Row style = {{width: 450, marginLeft: 10, marginTop: 10}}>
                                 300 Likes
                             </Row>
                            
-                                <InputGroup style = {{width: 450, marginLeft: 511, marginTop: 10}}>
+                                <InputGroup style = {{width: 320, marginLeft: 10, marginTop: 10}}>
                                     <Input placeholder = "Add a comment..." />
-                                </InputGroup>
-                        
-                            
+                                </InputGroup>  
                     </small>
         
             
                   </CardText>
+                  </Media>
+
+                  <Media query = "(max-width: 400px)">
+                  <CardText>
+                    <small className="text-muted">
+                        <Row style = {{width: 450, marginLeft: 10, marginTop: 10}}>
+                            <img src = "./images/like.svg" style = {{width: 19}}/>
+                            <img src = "./images/chat.svg" style = {{width: 19, marginLeft: 20}}/>
+                        </Row>
+                    </small>
+                    <small className = "text-muted">
+                            <Row style = {{width: 450, marginLeft: 10, marginTop: 10}}>
+                                300 Likes
+                            </Row>
+                           
+                                <InputGroup style = {{width: 320, marginLeft: 10, marginTop: 10}}>
+                                    <Input placeholder = "Add a comment..." />
+                                </InputGroup>  
+                    </small>
+        
+            
+                  </CardText>
+                  </Media>
                 </CardBody>
               </Card>
             </div>
