9166458f0c50f6245db6ee87c9abdda3ae32f71a
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-05-02 10:38:24 -0400

    restructured nav

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Media from 'react-media';
 import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Navs';
+import Form from 'react-bootstrap/Form';
 
 class SearchBar extends React.Component {
   constructor(props) {
@@ -19,102 +21,18 @@ class SearchBar extends React.Component {
   }
   render() {
     return (
-      <div className = "thisone">
-        
-        <Media query = "(max-width: 400px)">
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/"><img src = "./images/instagram.png" style = {{width: 130, paddingTop: 5, paddingLeft: 30}}/></NavbarBrand>
-          <Col xs="9">
-          </Col>
-        </Navbar>
-        </Media>
-
-        <Media query = "(min-width: 401px, max-width: 960px)">
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/"><img src = "./images/instagram2.png" style = {{width: 30}}/></NavbarBrand>
-          <NavbarBrand href="/"><img src = "./images/instagram.png" style = {{width: 130, paddingTop: 5, paddingLeft: 30}}/></NavbarBrand>
-          <Col xs="9">
-          <NavbarBrand>
-            <InputGroup>
-                <Input placeholder = "Search" />
-            </InputGroup>
-          </NavbarBrand>
-          </Col>
-
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/"><img src = "./images/navigator.svg" style = {{width: 25}}/></NavLink>
-              </NavItem>
-              <NavItem style = {{paddingLeft: 15}}>
-                <NavLink href="/"><img src = "./images/heart-like.svg" style = {{width: 25}}/></NavLink>
-              </NavItem>
-              <UncontrolledDropdown style = {{paddingLeft: 15}} nav inNavbar>
-                <DropdownToggle nav caret >
-                  <img src = "./images/user.svg" style = {{width: 25}}/>
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                  Profile
-                  </DropdownItem>
-                  <DropdownItem>
-                  Messages
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Logout
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar>
-        </Media>
-
-        <Media query = "(min-width: 961px)">
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/"><img src = "./images/instagram2.png" style = {{width: 30}}/></NavbarBrand>
-          <NavbarBrand href="/"><img src = "./images/instagram.png" style = {{width: 130, paddingTop: 5, paddingLeft: 30}}/></NavbarBrand>
-          <Col xs="9">
-          <NavbarBrand>
-            <InputGroup>
-                <Input placeholder = "Search" />
-            </InputGroup>
-          </NavbarBrand>
-          </Col>
-
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/"><img src = "./images/navigator.svg" style = {{width: 25}}/></NavLink>
-              </NavItem>
-              <NavItem style = {{paddingLeft: 15}}>
-                <NavLink href="/"><img src = "./images/heart-like.svg" style = {{width: 25}}/></NavLink>
-              </NavItem>
-              <UncontrolledDropdown style = {{paddingLeft: 15}} nav inNavbar>
-                <DropdownToggle nav caret >
-                  <img src = "./images/user.svg" style = {{width: 25}}/>
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                  Profile
-                  </DropdownItem>
-                  <DropdownItem>
-                  Messages
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Logout
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar>
-        </Media>
-      </div>
+        <Navbar bg="light" variant="light">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-primary">Search</Button>
+        </Form>
+      </Navbar>
     );
   }
 }
