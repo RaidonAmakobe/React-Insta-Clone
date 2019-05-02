c519ab37339182fc992e5b57ad915a07f4fc7155
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-05-02 01:46:57 -0400

    alignment

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 import { Card, Button, CardHeader, CardFooter, CardBody,
   CardTitle, CardText } from 'reactstrap';
+import { wrap } from 'module';
 
 class PostContainer extends React.Component {
     constructor(props) {
@@ -16,23 +18,24 @@ class PostContainer extends React.Component {
                   <CardTitle><img src = "./images/audric.jpg" style = {{width: 450}}/></CardTitle>
                   <CardText>
                     <small className="text-muted">
-                        <Col sm = "9">
+                        <Row style = {{width: 450, marginLeft: 520, marginTop: 10}}>
                             <img src = "./images/like.svg" style = {{width: 19}}/>
                             <img src = "./images/chat.svg" style = {{width: 19, marginLeft: 20}}/>
-                        </Col>
+                        </Row>
                     </small>
-                    <small className="text-muted">
-                        <Col sm = "9">
-                        <Input placeholder = "Search" />
-                        </Col>
+                    <small className = "text-muted">
+                            <Row style = {{width: 450, marginLeft: 520, marginTop: 10}}>
+                                300 Likes
+                            </Row>
+                           
+                                <InputGroup style = {{width: 450, marginLeft: 511, marginTop: 10}}>
+                                    <Input placeholder = "Add a comment..." />
+                                </InputGroup>
+                        
+                            
                     </small>
-                  </CardText>
-                  <CardText>
-                    <small className="text-muted">
-                        <Col sm="9">
-                            300 likes
-                        </Col>
-                   </small>
+        
+            
                   </CardText>
                 </CardBody>
               </Card>
