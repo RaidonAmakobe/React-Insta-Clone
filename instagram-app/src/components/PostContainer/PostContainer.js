4034777cd70e3a878caf020401cc778497956646
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-05-02 09:40:13 -0400

    more container styles / import bootstrap card
    - restructured card

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -3,8 +3,7 @@ import Image from 'react-bootstrap/Image'
 import Media from 'react-media';
 import { Container, Row, Col } from 'reactstrap';
 import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
-import { Card, Button, CardHeader, CardFooter, CardBody,
-  CardTitle, CardText } from 'reactstrap';
+import Card from 'react-bootstrap/Card'
 import { wrap } from 'module';
 
 class PostContainer extends React.Component {
@@ -14,76 +13,19 @@ class PostContainer extends React.Component {
     render(){
         return (
             <div className = 'container'>
-              <Card>
-                <CardHeader><img src = "./images/circleimg.png" style = {{width: 30, paddingRight: 10}}/>mrtestperson</CardHeader>
-                <CardBody>
-                  <CardTitle><Image src = "./images/audric.jpg" fluid style = {{width: 450}}/></CardTitle>
-
-                  <Media query = "(max-width: 400px)">
-                  <CardText>
-                    <small className="text-muted">
-                        <Row style = {{width: 450, marginLeft: 10, marginTop: 10}}>
-                            <img src = "./images/like.svg" style = {{width: 19}}/>
-                            <img src = "./images/chat.svg" style = {{width: 19, marginLeft: 20}}/>
-                        </Row>
-                    </small>
-                    <small className = "text-muted">
-                            <Row style = {{width: 450, marginLeft: 10, marginTop: 10}}>
-                                300 Likes
-                            </Row>
-                           
-                                <InputGroup style = {{width: 320, marginLeft: 10, marginTop: 10}}>
-                                    <Input placeholder = "Add a comment..." />
-                                </InputGroup>  
-                    </small>
-        
-            
-                  </CardText>
-                  </Media>
-
-                  <Media query = "(min-width: 401px, max-width: 960px)">
-                  <CardText>
-                    <small className="text-muted">
-                        <Row style = {{width: 450, marginLeft: 140, marginTop: 10}}>
-                            <img src = "./images/like.svg" style = {{width: 19}}/>
-                            <img src = "./images/chat.svg" style = {{width: 19, marginLeft: 20}}/>
-                        </Row>
-                    </small>
-                    <small className = "text-muted">
-                            <Row style = {{width: 450, marginLeft: 140, marginTop: 10}}>
-                                300 Likes
-                            </Row>
-                           
-                                <InputGroup  style = {{width: 440, marginLeft: 140, marginTop: 10}}>
-                                    <Input placeholder = "Add a comment..." />
-                                </InputGroup>  
-                    </small>
-        
-            
-                  </CardText>
-                  </Media>
-
-                  <Media query = "(min-width: 961px)">
-                  <CardText>
-                    <small className="text-muted">
-                        <Row style = {{width: 450, marginLeft: 515, marginTop: 10}}>
-                            <img src = "./images/like.svg" style = {{width: 19}}/>
-                            <img src = "./images/chat.svg" style = {{width: 19, marginLeft: 20}}/>
-                        </Row>
-                    </small>
-                    <small className = "text-muted">
-                            <Row style = {{width: 450, marginLeft: 515, marginTop: 10}}>
-                                300 Likes
-                            </Row>
-                           
-                                <InputGroup style = {{width: 450, marginLeft: 515, marginTop: 10}}>
-                                    <Input placeholder = "Add a comment..." />
-                                </InputGroup>  
-                    </small>
-                  </CardText>
-                  </Media>
-                </CardBody>
-              </Card>
+                <Card style = {{ marginTop: 50 }}>
+                    <Card.Img variant="top" src="./images/circleimg.png" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This card has supporting text below as a natural lead-in to additional
+                            content.{' '}
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
             </div>
         )
     }
